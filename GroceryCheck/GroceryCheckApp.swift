3626db744b8ddbf675f9de5c86ebbc8a20d186da
David Mclean <47851457+davupls@users.noreply.github.com>
2023-06-30 01:30:01 -0500

    Initial Commit

---
+++ GroceryCheck/GroceryCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryCheckApp.swift
+//  GroceryCheck
+//
+//  Created by David Mclean on 6/30/23.
+//
+
+import SwiftUI
+
+@main
+struct GroceryCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
