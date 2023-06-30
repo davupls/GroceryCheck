29e618e41193cd9dfa9ebcf347ac1e8ab7a98aac
David Mclean <47851457+davupls@users.noreply.github.com>
2023-06-30 01:38:12 -0500

    Foundation
    - Model using SwiftData
    - Users can add and delete items by swiping
    - Data is store in persistent memory

---
+++ GroceryCheck/CheckboxView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckboxView.swift
+//  GroceryCheck
+//
+//  Created by David Mclean on 6/30/23.
+//
+
+import SwiftUI
+
+struct CheckboxView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckboxView()
+}
---
+++ GroceryCheck/GroceryModel.swift
@@ -0,0 +1,8 @@
+//
+//  GroceryModel.swift
+//  GroceryCheck
+//
+//  Created by David Mclean on 6/30/23.
+//
+
+import Foundation
